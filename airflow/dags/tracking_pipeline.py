f955837254d1ff84a8321c3dcfcdceb2805ee308
DeepranjanG <kdeepu070@gmail.com>
2022-12-12 12:57:23 +0530

    airflow-single-pipeline-done

--- airflow/dags/tracking_pipeline.py
+++ airflow/dags/tracking_pipeline.py
@@ -32,97 +32,107 @@ with DAG(
 
     # [START extract_function]
 
-    from src.pipeline.tracking import TrackingPipeline
 
-    tracking_pipeline = TrackingPipeline()
-
-
-    def model_ingestion(**kwargs):
-        ti = kwargs['ti']
-        model_ingestion_artifacts = tracking_pipeline.start_model_ingestion()
-        print(model_ingestion_artifacts)
-        ti.xcom_push('model_ingestion_artifacts', model_ingestion_artifacts.to_dict())
-
-    def model_loading(**kwargs):
-        from src.entity.artifact_entity import ModelIngestionArtifacts
-        ti = kwargs['ti']
-        model_ingestion_artifacts = ti.xcom_pull(task_ids="model_ingestion", key="model_ingestion_artifacts")
-        model_ingestion_artifacts = ModelIngestionArtifacts(**(model_ingestion_artifacts))
-        print(model_ingestion_artifacts)
-        model_loading_artifacts = tracking_pipeline.start_model_loading(
-            model_ingestion_artifacts=model_ingestion_artifacts
-        )
-        ti.xcom_push('model_loading_artifacts', model_loading_artifacts.to_dict())
+    def tracking(**kwargs):
+        from src.pipeline.tracking import TrackingPipeline
+        TrackingPipeline().run_pipeline()
 
 
-    def data_transformation(**kwargs):
-        from src.entity.artifact_entity import ModelLoadingArtifacts
-        ti = kwargs['ti']
-        model_loading_artifacts = ti.xcom_pull(task_ids="model_loading", key="model_loading_artifacts")
-        model_loading_artifacts = ModelLoadingArtifacts(**(model_loading_artifacts))
-
-        data_transformation_artifacts = tracking_pipeline.start_data_transformation(
-            model_loading_artifacts=model_loading_artifacts
-        )
-        ti.xcom_push('data_transformation_artifacts', data_transformation_artifacts.to_dict())
-
-    def object_tracking(**kwargs):
-        from src.entity.artifact_entity import DataTransformationArtifacts, ModelLoadingArtifacts
-        ti = kwargs['ti']
-        data_transformation_artifacts = ti.xcom_pull(task_ids="data_transformation", key="data_transformation_artifacts")
-        data_transformation_artifacts = DataTransformationArtifacts(**(data_transformation_artifacts))
-
-        model_loading_artifacts = ti.xcom_pull(task_ids="model_loading", key="model_loading_artifacts")
-        model_loading_artifacts = ModelLoadingArtifacts(*(model_loading_artifacts))
-
-        object_tracking_artifacts = tracking_pipeline.start_object_tracking(
-            data_transformation_artifacts=data_transformation_artifacts,
-            model_loading_artifacts=model_loading_artifacts
-        )
-        ti.xcom_push('object_tracking_artifacts', object_tracking_artifacts.to_dict())
-
-    def pusher(**kwargs):
-        ti = kwargs['ti']
-        object_tracking_artifacts = ti.xcom_pull(task_ids="pusher", key="object_tracking_artifacts")
-
-        pusher_artifacts = tracking_pipeline.start_pusher()
-        print(f'Model pusher artifacts: {pusher_artifacts}')
-        ti.xcom_push('object_tracking_artifacts', object_tracking_artifacts.to_dict())
-
-        print("Tracking pipeline completed video saved to GCloud Storage")
-
-
-    model_ingestion = PythonOperator(
-        task_id='model_ingestion',
-        python_callable=model_ingestion,
-    )
-    model_ingestion.doc_md = dedent(
-        """\
-    #### Extract task
-    A simple Extract task to get data ready for the rest of the data pipeline.
-    In this case, getting data is simulated by reading from a hardcoded JSON string.
-    This data is then put into xcom, so that it can be processed by the next task.
-    """
-    )
-    model_loading = PythonOperator(
-        task_id="model_loading",
-        python_callable=model_loading
-    )
-
-    data_transformation = PythonOperator(
-        task_id="data_transformation",
-        python_callable=data_transformation
-    )
-
-    object_tracking = PythonOperator(
-        task_id="object_tracking",
-        python_callable=object_tracking
-
-    )
-    pusher = PythonOperator(
-        task_id="pusher",
-        python_callable=pusher
+    tracking_pipeline = PythonOperator(
+        task_id="track_pipeline",
+        python_callable=tracking
 
     )
 
-    model_ingestion >> model_loading >> data_transformation >> object_tracking >> pusher
+    tracking_pipeline
+
+    #
+    # def model_ingestion(**kwargs):
+    #     ti = kwargs['ti']
+    #     model_ingestion_artifacts = tracking_pipeline.start_model_ingestion()
+    #     print(model_ingestion_artifacts)
+    #     ti.xcom_push('model_ingestion_artifacts', model_ingestion_artifacts.to_dict())
+    #
+    # def model_loading(**kwargs):
+    #     from src.entity.artifact_entity import ModelIngestionArtifacts
+    #     ti = kwargs['ti']
+    #     model_ingestion_artifacts = ti.xcom_pull(task_ids="model_ingestion", key="model_ingestion_artifacts")
+    #     model_ingestion_artifacts = ModelIngestionArtifacts(**(model_ingestion_artifacts))
+    #     print(model_ingestion_artifacts)
+    #     model_loading_artifacts = tracking_pipeline.start_model_loading(
+    #         model_ingestion_artifacts=model_ingestion_artifacts
+    #     )
+    #     ti.xcom_push('model_loading_artifacts', model_loading_artifacts.to_dict())
+    #
+    #
+    # def data_transformation(**kwargs):
+    #     from src.entity.artifact_entity import ModelLoadingArtifacts
+    #     ti = kwargs['ti']
+    #     model_loading_artifacts = ti.xcom_pull(task_ids="model_loading", key="model_loading_artifacts")
+    #     model_loading_artifacts = ModelLoadingArtifacts(**(model_loading_artifacts))
+    #
+    #     data_transformation_artifacts = tracking_pipeline.start_data_transformation(
+    #         model_loading_artifacts=model_loading_artifacts
+    #     )
+    #     ti.xcom_push('data_transformation_artifacts', data_transformation_artifacts.to_dict())
+    #
+    # def object_tracking(**kwargs):
+    #     from src.entity.artifact_entity import DataTransformationArtifacts, ModelLoadingArtifacts
+    #     ti = kwargs['ti']
+    #     data_transformation_artifacts = ti.xcom_pull(task_ids="data_transformation", key="data_transformation_artifacts")
+    #     data_transformation_artifacts = DataTransformationArtifacts(**(data_transformation_artifacts))
+    #
+    #     model_loading_artifacts = ti.xcom_pull(task_ids="model_loading", key="model_loading_artifacts")
+    #     model_loading_artifacts = ModelLoadingArtifacts(*(model_loading_artifacts))
+    #
+    #     object_tracking_artifacts = tracking_pipeline.start_object_tracking(
+    #         data_transformation_artifacts=data_transformation_artifacts,
+    #         model_loading_artifacts=model_loading_artifacts
+    #     )
+    #     ti.xcom_push('object_tracking_artifacts', object_tracking_artifacts.to_dict())
+    #
+    # def pusher(**kwargs):
+    #     ti = kwargs['ti']
+    #     object_tracking_artifacts = ti.xcom_pull(task_ids="pusher", key="object_tracking_artifacts")
+    #
+    #     pusher_artifacts = tracking_pipeline.start_pusher()
+    #     print(f'Model pusher artifacts: {pusher_artifacts}')
+    #     ti.xcom_push('object_tracking_artifacts', object_tracking_artifacts.to_dict())
+    #
+    #     print("Tracking pipeline completed video saved to GCloud Storage")
+    #
+    #
+    # model_ingestion = PythonOperator(
+    #     task_id='model_ingestion',
+    #     python_callable=model_ingestion,
+    # )
+    # model_ingestion.doc_md = dedent(
+    #     """\
+    # #### Extract task
+    # A simple Extract task to get data ready for the rest of the data pipeline.
+    # In this case, getting data is simulated by reading from a hardcoded JSON string.
+    # This data is then put into xcom, so that it can be processed by the next task.
+    # """
+    # )
+    # model_loading = PythonOperator(
+    #     task_id="model_loading",
+    #     python_callable=model_loading
+    # )
+    #
+    # data_transformation = PythonOperator(
+    #     task_id="data_transformation",
+    #     python_callable=data_transformation
+    # )
+    #
+    # object_tracking = PythonOperator(
+    #     task_id="object_tracking",
+    #     python_callable=object_tracking
+    #
+    # )
+    # pusher = PythonOperator(
+    #     task_id="pusher",
+    #     python_callable=pusher
+    #
+    # )
+    #
+    # model_ingestion >> model_loading >> data_transformation >> object_tracking >> pusher
